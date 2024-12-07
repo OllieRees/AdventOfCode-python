c4de9374b1d6b1a0681606ac068b5f63cb42600b
Ollie Rees <oliver.x.rees@gmail.com>
2024-12-07 16:37:03 +0000

    Part 2 is a bit prettier now :)

--- src/mulledwine/main.py
+++ src/mulledwine/main.py
@@ -17,14 +17,23 @@ class TokenType(StrEnum):
         return next(type for type in cls if type.is_valid_token(token=token))
     
     @classmethod
-    def tokenise_all(cls, *, token_str: str) -> Generator["Token", None, None]:
+    def tokenise_all(cls, *, token_str: str) -> Iterator["Token"]:
         return Tokeniser(token_str=token_str, pattern=re.compile("|".join(r for r in cls))).tokenise()
     
-    def tokenise(self, *, token_str: str) -> Generator["Token", None, None]:
+    def tokenise(self, *, token_str: str) -> Iterator["Token"]:
         return Tokeniser(token_str=token_str, pattern=re.compile(self)).tokenise()
         
     def is_valid_token(self, *, token: str) -> bool:
         return re.compile(self).fullmatch(token) is not None
+    
+    def is_enabled(self, *, is_enabled: bool) -> bool:
+        match self:
+            case TokenType.MUL:
+                return is_enabled
+            case TokenType.DONT:
+                return False
+            case _:
+                return True
 
 
 class Tokeniser:
@@ -32,30 +41,41 @@ class Tokeniser:
         self.token_str = token_str
         self.pattern = pattern
     
-    def tokenise(self) -> Generator["Token", None, None]:
+    def tokenise(self) -> Iterator["Token"]:
+        rv = []
+        is_enabled = True
         for token in self.pattern.finditer(self.token_str):
             if type := TokenType.from_token_str(token=token.group()):
-                yield Token(token=token.group(), type=type)
-        return None
-
+                is_enabled = type.is_enabled(is_enabled=is_enabled)
+                rv.append(Token(token=token.group(), type=type, is_enabled=is_enabled))
+        return iter(rv)
+    
 
 class Token:
-    def __init__(self, *, token: str, type: TokenType):
+    def __init__(self, *, token: str, type: TokenType, is_enabled: bool = True):
         if not type.is_valid_token(token=token):
             raise ValueError(f"Invalid Token for Pattern. Token={token} Pattern={type}")
         self.type = type
         self.token = token
+        self.is_enabled = is_enabled
 
     @property
     def args(self) -> Iterator[str]:
         return iter(next(re.compile(self.type).finditer(self.token)).groups())
     
+    @property
+    def result(self) -> int:
+        match self.type:
+            case TokenType.MUL:
+                return Multiply(token=self.token, is_enabled=self.is_enabled).result
+        return 0
+    
 
 class Multiply(Token):
     type = TokenType.MUL
 
-    def __init__(self, *, token: str):
-        super().__init__(token=token, type=self.type)
+    def __init__(self, *, token: str, is_enabled: bool = True):
+        super().__init__(token=token, type=self.type, is_enabled=is_enabled)
         self.l = next(self._args)
         self.r = next(self._args)
 
@@ -77,14 +97,14 @@ class Multiply(Token):
     @classmethod
     def tokenise(cls, *, token_str: str) -> Generator["Multiply", None, None]:
         for token in cls.type.tokenise(token_str=token_str):
-            yield Multiply(token=token.token)
+            yield Multiply(token=token.token, is_enabled=token.is_enabled)
 
 
 class Do(Token):
     type = TokenType.DO
 
     def __init__(self, *, token: str):
-        super().__init__(token=token, type=self.type)
+        super().__init__(token=token, type=self.type, is_enabled=True)
         
     def __str__(self) -> str:
         return "do"
@@ -94,14 +114,13 @@ class Dont(Token):
     type = TokenType.DONT
 
     def __init__(self, *, token: str):
-        super().__init__(token=token, type=self.type)
+        super().__init__(token=token, type=self.type, is_enabled=False)
         
     def __str__(self) -> str:
         return "don't"
 
 
 def main(lines: Iterator[str]) -> None:
-    tokens = [token for line in lines for token in TokenType.tokenise_all(token_str=line)]
-    print(f"Token Sum: {sum(Multiply(token=token.token).result for token in tokens if token.type == TokenType.MUL)}")
-
-    # skip any muls between a don't and a do
+    tokens = [token for token in TokenType.tokenise_all(token_str="".join(lines))]
+    print(f"Token Sum: {sum(token.result for token in tokens)}")
+    print(f"Enabled Tokens Sum: {sum(token.result for token in tokens if token.is_enabled)}")
