6f73c68f3acdb1b264fda3e9200a0d5621d71b92
Ollie Rees <oliver.x.rees@gmail.com>
2025-12-01 17:08:15 +0000

    Dial class that can rotate

--- src/secret_entrance/__init__.py
+++ src/secret_entrance/__init__.py
@@ -4,4 +4,10 @@ from secret_entrance import main as SecretEntrance
 
 
 def main(lines: Iterator[str]) -> None:
-    print([step for step in SecretEntrance.StepsConverter().from_lines(lines)])
+    dial = SecretEntrance.Dial(current_position=50)
+    steps = SecretEntrance.StepsConverter().from_lines(lines)
+    positions: list[int] = []
+    for step in steps:
+        dial.rotate(step)
+        positions.append(dial.current_position)
+    print(len([1 for pos in positions if pos == 0]))
--- src/secret_entrance/main.py
+++ src/secret_entrance/main.py
@@ -3,6 +3,31 @@ from enum import StrEnum
 from typing import Iterator
 
 
+class Dial:
+    def __init__(self, current_position: int = 0) -> None:
+        self._current_position = current_position
+
+    @property
+    def current_position(self) -> int:
+        return self._current_position
+
+    @current_position.setter
+    def current_position(self, value: int) -> None:
+        if not (0 <= value <= 99):
+            raise ValueError("Position must be between 0 and 99 inclusive.")
+        self._current_position = value
+
+    def rotate(self, step: "Step") -> None:
+        match step.direction:
+            case Direction.LEFT:
+                new_position = (self.current_position - step.magnitude) % 100
+            case Direction.RIGHT:
+                new_position = (self.current_position + step.magnitude) % 100
+            case _:
+                raise ValueError("Invalid direction.")
+        self._current_position = new_position
+
+
 class Direction(StrEnum):
     LEFT = "L"
     RIGHT = "R"
@@ -18,7 +43,3 @@ class StepsConverter:
     def from_lines(self, lines: Iterator[str]) -> Iterator[Step]:
         for line in lines:
             yield Step(direction=Direction(line[0].upper()), magnitude=int(line[1:]))
-
-
-def main(lines: Iterator[str]) -> None:
-    print([step for step in StepsConverter().from_lines(lines)])
